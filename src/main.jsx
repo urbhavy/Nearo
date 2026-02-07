d3104e1ff3b4c0aaf2ddacabf9ef95b0c3b0caeb
urbhavy <chauahanbhavy@gmail.com>
2026-02-07 19:41:10 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import { LocationProvider } from './context/LocationContext'
 
-import ErrorBoundary from './components/ErrorBoundary'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
